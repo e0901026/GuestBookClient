2e03ff161249ae95b3dd7cbecfe1592679e67e86
570 <baiqi.wu@devatus.fi>
2013-03-22 09:48:26 +0200

    IOS version
    GuestBook iOS version developed by phone gap technology

---
+++ GuestBook/www/js/index.js
@@ -0,0 +1,15 @@
+var app = {
+    initialize: function() {
+        this.bind();
+    },
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function() {
+        // note that this is an event handler so the scope is that of the event
+        // so we need to call app.report(), and not this.report()
+        app.report(' ');
+    },	
+    report: function() { 
+    }
+};
